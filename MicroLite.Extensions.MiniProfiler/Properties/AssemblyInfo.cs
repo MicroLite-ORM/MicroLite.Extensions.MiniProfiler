1246c178679f448e25fac5bb88f24de960e53356
Trevor Pilley <trev@trevorpilley.co.uk>
2015-01-01 18:26:36 +0000

    Built MicroLite.Extensions.MiniProfiler 6.1.0

--- MicroLite.Extensions.MiniProfiler/Properties/AssemblyInfo.cs
+++ MicroLite.Extensions.MiniProfiler/Properties/AssemblyInfo.cs
@@ -44,9 +44,9 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
-// [assembly: AssemblyVersion("6.0.0.0")]
-[assembly: AssemblyVersion("6.0.0.0")]
-[assembly: AssemblyFileVersion("6.0.0.0")]
-[assembly: AssemblyInformationalVersion("6.0.0.30")]
+// [assembly: AssemblyVersion("6.1.0.0")]
+[assembly: AssemblyVersion("6.1.0.0")]
+[assembly: AssemblyFileVersion("6.1.0.0")]
+[assembly: AssemblyInformationalVersion("6.1.0.34")]
 [assembly: CLSCompliant(true)]
 [assembly: NeutralResourcesLanguageAttribute("en-GB")]
